54c27cc7b00c69ec66c15045bde396cf0aa37e8f
apohies <miguelegion@gmail.com>
2020-05-07 19:27:17 -0500

    cambios generales

--- src/App.js
+++ src/App.js
@@ -1,67 +1,19 @@
 import React, { Component } from 'react';
-import axios from "axios";
-import FormularioInstituto from './component/formularioColegio';
-import Caja1 from './component/caja1';
-import Datatable from './component/dataTable';
-import ModalMDB from './component/modalMDB/ModalMDB';
-import ImageClass from './component/image/ImageClass';
+import General from "./component/general";
+
 
 
 
 class App extends Component 
 {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            items: [],
-            isShowing: false
-        };
-        this.traerDatos = this.traerDatos.bind(this);
-
-    }
-
-
-    traerDatos() {
-        axios.get(`http://127.0.0.1:8000/api/institutions`)
-            .then(res => {
-                this.setState({ items: res.data['instituciones'] });
-            });
-
-    }
-
-
-    componentDidMount() {
-
-        this.traerDatos();
-
-    }
-
-    openModalHandler = () => {
-        this.setState({
-            isShowing: true
-        });
-    }
-
-    closeModalHandler = () => {
-        this.setState({
-            isShowing: false
-        });
-    }
+   
 
     render(){
         
         return <div className="card">
-                    <ModalMDB></ModalMDB>
-                    <ImageClass></ImageClass>
-                    <FormularioInstituto actualizar={this.traerDatos}></FormularioInstituto>
-                    <div className="card-header"> Todo los colegios</div>
-                    <div className="card-body">
-                        <div className="row">
-                            {this.state.items.map(intitution=><Caja1 parametros={intitution}></Caja1>)}
-                        </div>
-                        <Datatable></Datatable>   
-                    </div>
+                  
+                
                 </div>
     
     }
---
+++ src/component/general.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import axios from "axios";
+import FormularioInstituto from './formularioColegio';
+import Caja1 from './caja1';
+import Datatable from './dataTable';
+import ModalMDB from './modalMDB/ModalMDB';
+import ImageClass from './image/ImageClass';
+
+
+
+class General extends Component 
+{
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+            isShowing: false
+        };
+        this.traerDatos = this.traerDatos.bind(this);
+
+    }
+
+
+    traerDatos() {
+        axios.get(`http://127.0.0.1:8000/api/institutions`)
+            .then(res => {
+                this.setState({ items: res.data['instituciones'] });
+            });
+
+    }
+
+
+    componentDidMount() {
+
+        this.traerDatos();
+
+    }
+
+    openModalHandler = () => {
+        this.setState({
+            isShowing: true
+        });
+    }
+
+    closeModalHandler = () => {
+        this.setState({
+            isShowing: false
+        });
+    }
+
+    render(){
+        
+        return <div className="card">
+                    <ModalMDB></ModalMDB>
+                    <ImageClass></ImageClass>
+                    <FormularioInstituto actualizar={this.traerDatos}></FormularioInstituto>
+                    <div className="card-header"> Todo los colegios</div>
+                    <div className="card-body">
+                        <div className="row">
+                            {this.state.items.map(intitution=><Caja1 parametros={intitution}></Caja1>)}
+                        </div>
+                        <Datatable></Datatable>   
+                    </div>
+                </div>
+    
+    }
+}
+export default General;
